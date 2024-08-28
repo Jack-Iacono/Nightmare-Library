ca7e483fba03df751dec49ab2863ada90858d4b5
John (Jack) Iacono <90567244+Jack-Iacono@users.noreply.github.com>
2024-08-28 17:33:36 -0400

    Began Inventory System Setup

---
+++ Nightmare Library/Assets/Jack/_Scripts/Inventory Scripts/InventoryController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    private const int inventorySize = 3;
+    private InventoryItem[] inventoryItems = new InventoryItem[inventorySize];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Nightmare Library/Assets/Jack/_Scripts/Inventory Scripts/InventoryItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem
+{
+    public GameObject realObject;
+
+    public InventoryItem(GameObject g)
+    {
+        realObject = g;
+    }
+}

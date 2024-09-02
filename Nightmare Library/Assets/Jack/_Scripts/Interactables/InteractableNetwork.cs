97e101b0286ae008f0f568a5c2bece1448d246e7
John (Jack) Iacono <90567244+Jack-Iacono@users.noreply.github.com>
2024-09-02 16:11:54 -0400

    Added Network Stuff
    - Added Network capabilities for various interaction on the InteractableNetwork script

--- Nightmare Library/Assets/Jack/_Scripts/Interactables/InteractableNetwork.cs
+++ Nightmare Library/Assets/Jack/_Scripts/Interactables/InteractableNetwork.cs
@@ -17,6 +17,10 @@ public class InteractableNetwork : NetworkBehaviour
         parent = GetComponent<Interactable>();
 
         parent.OnClick += OnClick;
+        parent.OnPickup += OnPickup;
+        parent.OnPlace += OnPlace;
+        parent.OnEnemyInteractHysterics += OnEnemyInteractHysterics;
+        parent.OnEnemyInteractFlicker += OnEnemyInteractFlicker;
     }
 
     protected virtual void OnClick(bool fromNetwork = false)
@@ -36,6 +40,70 @@ public class InteractableNetwork : NetworkBehaviour
     }
     [ClientRpc]
     protected virtual void ConsumeClickClientRpc(ulong sender)
+    {
+        if (NetworkManager.LocalClientId != sender)
+            Debug.Log("Click on client " + sender);
+    }
+
+    protected virtual void OnPickup(bool fromNetwork)
+    {
+        throw new NotImplementedException();
+    }
+    [ServerRpc(RequireOwnership = false)]
+    protected virtual void TransmitPickupServerRpc(ulong sender)
+    {
+        ConsumePickupClientRpc(sender);
+    }
+    [ClientRpc]
+    protected virtual void ConsumePickupClientRpc(ulong sender)
+    {
+        if (NetworkManager.LocalClientId != sender)
+            Debug.Log("Click on client " + sender);
+    }
+
+    protected virtual void OnPlace(bool fromNetwork)
+    {
+        throw new NotImplementedException();
+    }
+    [ServerRpc(RequireOwnership = false)]
+    protected virtual void TransmitPlaceServerRpc(ulong sender)
+    {
+        ConsumePlaceClientRpc(sender);
+    }
+    [ClientRpc]
+    protected virtual void ConsumePlaceClientRpc(ulong sender)
+    {
+        if (NetworkManager.LocalClientId != sender)
+            Debug.Log("Click on client " + sender);
+    }
+
+    protected virtual void OnEnemyInteractHysterics(bool fromNetwork)
+    {
+        throw new NotImplementedException();
+    }
+    [ServerRpc(RequireOwnership = false)]
+    protected virtual void TransmitEnemyInteractHystericsServerRpc(ulong sender)
+    {
+        ConsumeEnemyInteractHystericsClientRpc(sender);
+    }
+    [ClientRpc]
+    protected virtual void ConsumeEnemyInteractHystericsClientRpc(ulong sender)
+    {
+        if (NetworkManager.LocalClientId != sender)
+            Debug.Log("Click on client " + sender);
+    }
+
+    protected virtual void OnEnemyInteractFlicker(bool fromNetwork)
+    {
+        throw new NotImplementedException();
+    }
+    [ServerRpc(RequireOwnership = false)]
+    protected virtual void TransmitEnemyInteractFlickerServerRpc(ulong sender)
+    {
+        ConsumeEnemyInteractFlickerClientRpc(sender);
+    }
+    [ClientRpc]
+    protected virtual void ConsumeEnemyInteractFlickerClientRpc(ulong sender)
     {
         if (NetworkManager.LocalClientId != sender)
             Debug.Log("Click on client " + sender);
